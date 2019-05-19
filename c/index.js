154edfc52eeb7d02eeb3ce26a3b5b3bf522becaa
loog245869 <33905772+loog245869@users.noreply.github.com>
2019-05-19 11:43:33 +0800

    Add files via upload

---
+++ c/index.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+/*function copyright_on(x){
+	var a=document.getElementById("end").children;	//获取子元素
+	a[0].style.display="none";
+	x.style.height="auto";
+	var b=document.getElementById("copyright-details");
+	b.style.display="block";
+}
+function copyright_off(x){
+	var a=document.getElementById("end").children;
+	a[0].style.display="block";
+	x.style.height="80px";
+	var b=document.getElementById("copyright-details");
+	b.style.display="none";
+}*/
